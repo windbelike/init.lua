97faa9c3b365429d73cb1e43da546e79d74a25af
sawyerrick <1103031078@qq.com>
2023-06-23 20:24:14 +0800

    add something cool

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -85,6 +85,11 @@ _G.packer_plugins = {
     path = "/Users/sawyer/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
+  ["auto-pairs"] = {
+    loaded = true,
+    path = "/Users/sawyer/.local/share/nvim/site/pack/packer/start/auto-pairs",
+    url = "https://github.com/jiangmiao/auto-pairs"
+  },
   ["cmp-buffer"] = {
     loaded = true,
     path = "/Users/sawyer/.local/share/nvim/site/pack/packer/start/cmp-buffer",
