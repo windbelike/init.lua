2bf4eedd4ee597be5b49b7eb002ead60ea2710e4
sawyerrick <1103031078@qq.com>
2023-07-13 22:59:42 +0800

    feat: recent buffers

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -63,6 +63,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    -- gl
     -- vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
     vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
     vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
